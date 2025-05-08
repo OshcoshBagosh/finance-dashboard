117f32acd87d6668ab9c1764cf6106077da28efd
Oscar Guerrero <oscargabrguerrero@gmai;.com>
2025-05-07 23:31:31 -0400

    Finished CRUD for budget class

--- backend/app/api/budget_routes.py
+++ backend/app/api/budget_routes.py
@@ -1,14 +1,15 @@
 from ..db.mongo import get_budget_collection
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from ..models.budget import Budget
 from typing import List, Optional
+from bson import ObjectId
 import datetime
 
 budget_collection = get_budget_collection()
 
 router = APIRouter()
 
-@router.post("/budgets/", response_model=Budget)
+@router.post("/", response_model=Budget)
 async def create_budget(budget: Budget):
     budget_dict = budget.dict()
     result = await budget_collection.insert_one(budget_dict)
@@ -16,7 +17,7 @@ async def create_budget(budget: Budget):
     budget_dict["_id"] = inserted_id
     return Budget(**budget_dict)
 
-@router.get("/budgets/", response_model=List[Budget])
+@router.get("/", response_model=List[Budget])
 async def get_budgets(user_id:str, month:Optional[datetime.datetime] = None):
     query = {"user_id": user_id}
     if month:
@@ -29,3 +30,47 @@ async def get_budgets(user_id:str, month:Optional[datetime.datetime] = None):
         results.append(Budget(**budget))
 
     return results
+
+@router.get("/{budget_id}", response_model=Budget)
+async def get_budget(budget_id:str):
+    try:
+        budget = await budget_collection.find_one({"_id": ObjectId(budget_id)})
+
+        if not ObjectId.is_valid(budget_id):
+            raise HTTPException(status_code=404, detail="Budget not found")
+        budget["_id"] = str(budget["_id"])
+        return Budget(**budget)
+    
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid ID format")
+
+@router.put("/{budget_id}", response_model=Budget)
+async def update_budget(budget_id:str, budget:Budget):
+    if not ObjectId.is_valid(budget_id):
+            raise HTTPException(status_code=404, detail="Budget not found")
+    update = budget.dict(exclude_unset=True, by_alias=True)
+    update.pop("_id", None)
+
+    result = await budget_collection.update_one(
+        {"_id": ObjectId(budget_id)},
+        {"$set": update}
+    )
+
+    if result.modified_count == 0:
+        raise HTTPException(status_code=404, detail="Budget not found or unchanged")
+    
+    updated = await budget_collection.find_one({"_id": ObjectId(budget_id)})
+    updated["_id"] = str(updated["_id"])
+    return Budget(**updated)
+
+@router.delete("/{budget_id}", response_model=Budget)
+async def delete_budget(budget_id:str):
+    if not ObjectId.is_valid(budget_id):
+        raise HTTPException(status_code=400, detail="Invalid ID format")
+    
+    result = await budget_collection.find_one_and_delete({"_id": ObjectId(budget_id)})
+    if result is None:
+        raise HTTPException(status_code=404, detail="Budget not found")
+    result["_id"] = str(result["_id"])
+
+    return Budget(**result)
